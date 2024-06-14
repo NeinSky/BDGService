596c94d9905f57b3d5a243a25a15cb6a608e61b8
Artem Pronchenko <xiii2005@ya.ru>
2024-06-14 12:42:33 +0300

    Pre-release. README.md added.

--- backend/config.py
+++ backend/config.py
@@ -17,7 +17,7 @@ DB_PORT = '5432'
 DB_ECHO = False
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
+DB_NAME = 'bdgs'
 
 
 # Настройки аутентификации
